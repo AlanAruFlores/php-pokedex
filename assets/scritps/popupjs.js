e4edc974db86f93d7e3a4a2dcf655a29c71aa72d
NotaMateria <fran.caco.fv@gmail.com>
2024-04-30 20:15:43 -0300

    login

--- assets/scritps/popupjs.js
+++ assets/scritps/popupjs.js
@@ -8,14 +8,27 @@ document.addEventListener('DOMContentLoaded', function() {
         const usuario = document.getElementById('usuario').value;
         const contrasena = document.getElementById('contrasena').value;
 
-        const usuario_valido = 'admin';
-        const contrasena_valida = 'admin';
+        // Realizar solicitud AJAX para obtener las credenciales desde el archivo PHP
+        fetch('credenciales.php')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error al obtener las credenciales');
+                }
+                return response.json();
+            })
+            .then(credenciales => {
+                const usuario_valido = credenciales.usuario;
+                const contrasena_valida = credenciales.contrasena;
 
-        if (usuario === usuario_valido && contrasena === contrasena_valida) {
-            const contenidoSecreto = document.querySelector('.popup');
-            contenidoSecreto.style.display = 'none';
-        } else {
-            mensajeError.style.display = 'block';
-        }
+                if (usuario === usuario_valido && contrasena === contrasena_valida) {
+                    const contenidoSecreto = document.querySelector('.popup');
+                    contenidoSecreto.style.display = 'none';
+                } else {
+                    mensajeError.style.display = 'block';
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
     });
 });
