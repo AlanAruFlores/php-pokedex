948f990765e0a0028472ab15003938305e7dabac
AlanAruFlores <alanaruquipa242@gmail.com>
2024-05-21 19:36:11 -0300

    eliminando archivos viejos

--- php/cerrar_sesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $ruta_archivo = parse_ini_file("../config.ini");
-    $url = $ruta_archivo["URL"];
-    session_start();
-    session_destroy();
-
-    header("Location:$url/index.php");
-?>
--- php/obtener_un_pokemon.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-
-    function obtenerUnPokemonPorId($id, $conexion){
-       $sql = "SELECT * FROM pokemon WHERE id = $id";
-       $result = mysqli_query($conexion, $sql);
-       return mysqli_fetch_assoc( $result );
-    }
-?>
